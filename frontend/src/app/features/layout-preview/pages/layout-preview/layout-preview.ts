57b20fe4fc6ed6ef3f09df0758bba2f1b6e5a1fc
Aman Agrawal <aman007agg@gmail.com>
2026-07-13 00:06:19 +0530

    phase-4: implement AI layout comparison screen

--- frontend/src/app/features/layout-preview/pages/layout-preview/layout-preview.ts
+++ frontend/src/app/features/layout-preview/pages/layout-preview/layout-preview.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout-preview',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatIconModule
+  ],
   templateUrl: './layout-preview.html',
-  styleUrl: './layout-preview.scss',
+  styleUrl: './layout-preview.scss'
 })
-export class LayoutPreview {}
+export class LayoutPreview {
+
+  selectedLayout = 'A';
+
+}
