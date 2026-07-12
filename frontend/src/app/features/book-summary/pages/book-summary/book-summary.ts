d96a83cd91cd8c56bfa48418349c72f3fa51fe00
Aman Agrawal <aman007agg@gmail.com>
2026-07-13 00:14:09 +0530

    phase-4: implement book summary screen

--- frontend/src/app/features/book-summary/pages/book-summary/book-summary.ts
+++ frontend/src/app/features/book-summary/pages/book-summary/book-summary.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-book-summary',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './book-summary.html',
-  styleUrl: './book-summary.scss',
+  styleUrl: './book-summary.scss'
 })
-export class BookSummary {}
+export class BookSummary {
+
+  bookTitle = 'AI Handbook';
+
+  totalPages = 8;
+
+  layoutsSelected = 8;
+
+}
