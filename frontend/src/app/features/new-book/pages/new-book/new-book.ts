c126ba8156a5b9c58f04b6a8b2067ba496e92213
Aman Agrawal <aman007agg@gmail.com>
2026-07-12 22:32:20 +0530

    phase-4: implement home and new book screens with angular material

--- frontend/src/app/features/new-book/pages/new-book/new-book.ts
+++ frontend/src/app/features/new-book/pages/new-book/new-book.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-new-book',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
   templateUrl: './new-book.html',
-  styleUrl: './new-book.scss',
+  styleUrl: './new-book.scss'
 })
-export class NewBook {}
+export class NewBook {
+
+  title = '';
+
+  objective = '';
+
+}
